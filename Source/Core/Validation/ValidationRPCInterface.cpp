50f87701c7c61eda067dafb39d1939028ee63d4e
Randal A. Koene <rkoene@carboncopies.org>
2024-04-22 09:54:51 -0700

    Added interface stub.

---
+++ Source/Core/Validation/ValidationRPCInterface.cpp
@@ -0,0 +1,30 @@
+#include <ValidationRPCInterface.h>
+#include <RPC/APIStatusCode.h>
+
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+#include <thread>
+#include <mutex>
+
+namespace BG {
+
+ValidationRPCInterface::ValidationRPCInterface(BG::Common::Logger::LoggingSystem* _Logger, API::RPCManager* _RPCManager) {
+    assert(_Logger != nullptr);
+    assert(_RPCManager != nullptr);
+
+    Logger_ = _Logger;
+
+    // Register Callbacks
+    _RPCManager->AddRoute("Validation/SCValidation", std::bind(&ValidationRPCInterface::SCValidation, this, std::placeholders::_1));
+
+}
+
+ValidationRPCInterface::~ValidationRPCInterface() {
+}
+
+std::string ValidationRPCInterface::SCValidation(std::string _JSONRequest) {
+    
+}
+
+} // BG
---
+++ Source/Core/Validation/ValidationRPCInterface.h
@@ -0,0 +1,66 @@
+//===========================================================//
+// This file is part of the BrainGenix-NES Validation System //
+//===========================================================//
+
+/*
+    Description: This file provides the validation RPC interfaces.
+    Additional Notes: None
+    Date Created: 2024-04-22
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <assert.h>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <nlohmann/json.hpp>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <RPC/RPCManager.h>
+#include <RPC/RPCHandlerHelper.h>
+
+#include <BG/Common/Logger/Logger.h>
+
+
+namespace BG {
+
+/**
+ * @brief This class provides the infrastructure to run simulations.
+ */
+class ValidationRPCInterface {
+
+private:
+
+    BG::Common::Logger::LoggingSystem* Logger_ = nullptr; /**Pointer to the instance of the logging system*/
+
+
+public:
+
+    /**
+     * @brief Construct a new Interface object
+     * Give this a pointer to an initialized configuration object.
+     * 
+     * @param _Logger Pointer to the logging interface
+     * @param _Config 
+     * @param _RPCManager
+     */
+    ValidationRPCInterface(BG::Common::Logger::LoggingSystem* _Logger, API::RPCManager* _RPCManager);
+
+    ~ModelRPCInterface();
+
+    /**
+     * @brief Various routes for API
+     * 
+     * @param _JSONRequest 
+     * @return std::string 
+     */
+    std::string SCValidation(std::string _JSONRequest);
+
+};
+
+}; // Close Namespace BG
