63491b157ffc2cd4af1878e43948e1b3a648dd43
Randal A. Koene <rkoene@carboncopies.org>
2024-04-23 09:26:04 -0700

    N1Metrics todos.

--- Source/Core/Metrics/N1Metrics.cpp
+++ Source/Core/Metrics/N1Metrics.cpp
@@ -14,8 +14,20 @@
 namespace BG {
 
 bool N1Metrics::Validate() {
-	
-	
+
+
+	// Derive the connectome (connectivity matrix).
+
+	// As needed, convert this to two di-graphs, one for the KGT, one for the EMU.
+
+	// Calculate the edit distance.
+
+	// Calculate the Jaccard distance.
+
+	// Calculate the Quantum JSD.
+
+	// Generate report.
+
 	return true;
 }
 
